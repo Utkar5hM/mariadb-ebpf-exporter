10859d484517e3210b7e624ab7b707cae1db587d
ut <utkarshrm568@gmail.com>
2023-09-09 16:32:37 +0530

    sample prometheus exporter code

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
-	fmt.Println("Hello There")
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
